8a92f35df1f25ee8851ddd6c2b74b2d9301193ed
borvelt <borvelt@gmail.com>
2018-10-07 23:04:00 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# oauth2-server
+
+_oauth2 implementation - rfc6749, rfc7009, rfc7662_
+
+*Write your own project README*
---
+++ webpack.config/dist.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: { index: __dirname + '/../src/index.js' },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js',
+    chunkFilename: '[name].oauth2Server.js',
+    publicPath: '/',
+    library: 'oauth2Server',
+    globalObject: 'this',
+    libraryTarget: 'umd',
+  },
+  externals: [],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            '@babel/plugin-transform-runtime',
+            '@babel/plugin-syntax-dynamic-import',
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      root: path.join(__dirname, '..'),
+    }),
+    new UglifyJsPlugin(),
+  ],
+  resolve: {
+    alias: {},
+  },
+}
